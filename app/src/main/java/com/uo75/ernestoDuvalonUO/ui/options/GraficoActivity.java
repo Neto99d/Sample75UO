6fb3600fda54bea6eb4d57c7e94028680bc45fac
Neto99d <ernesto.duvalon@gmail.com>
2022-08-29 02:55:36 -0400

    Agregado grafico de pastel para estadisticas

--- app/src/main/java/com/uo75/ernestoDuvalonUO/ui/options/GraficoActivity.java
+++ app/src/main/java/com/uo75/ernestoDuvalonUO/ui/options/GraficoActivity.java
@@ -1,23 +1,23 @@
 package com.uo75.ernestoDuvalonUO.ui.options;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.uo75.ernestoDuvalonUO.R;
 import com.uo75.ernestoDuvalonUO.ui.RestClient;
 import com.uo75.ernestoDuvalonUO.ui.modelsOdoo.AccesOdoo;
 import com.uo75.ernestoDuvalonUO.ui.modelsOdoo.Data;
-import com.uo75.ernestoDuvalonUO.ui.modelsOdoo.Estadisticas;
-import com.uo75.ernestoDuvalonUO.ui.options.Adapters.EstadisticasAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,29 +29,35 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class EstadisticasActivity extends AppCompatActivity {
+public class GraficoActivity extends AppCompatActivity {
 
-    private EstadisticasAdapter nAdapter;
-    private RecyclerView mRecyclerView;
-
+    PieDataSet pastelData = new PieDataSet(null, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grafico);
 
-        setContentView(R.layout.activity_estadisticas);
-        mRecyclerView = findViewById(R.id.recycler_estadisticas);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mRecyclerView.setHasFixedSize(true);
-        Button button = findViewById(R.id.verGraficoBton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), GraficoActivity.class);
-                startActivity(intent);
-            }
-        });
+        Toast toast = Toast.makeText(this, "Toque la pantalla para mostrar el gráfico con los datos.", Toast.LENGTH_LONG);
+        toast.show();
+        PieChart pieChart = findViewById(R.id.pastelChart);
+
+        /////////////////////////////////////////////
+        pastelData.setColors(ColorTemplate.COLORFUL_COLORS);
+        pastelData.setValueTextColor(Color.BLACK);
+        pastelData.setValueTextSize(19f);
+
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Estadísticas UO");
+        pieChart.getLegend().setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        pieChart.getLegend().setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        pieChart.animate();
+        pastelData.setFormLineWidth(4);
+
+        //////////////////////////////////////////////////////
+
+
+        /////////////////////////////////////////////////////////////////
 
         //Definimos la URL base del API REST que utilizamos
         String baseUrl = "http://192.168.1.2:8069/";
@@ -87,7 +93,6 @@ public class EstadisticasActivity extends AppCompatActivity {
                     //Definimos la URL base del API REST que utilizamos
                     String baseUrl = "http://192.168.1.2:8069/";
 
-                    ArrayList<Estadisticas> estadisticas = new ArrayList<>();
                     //Instancia a GSON
                     Gson gson = new GsonBuilder()
                             .setDateFormat("yyyy-MM-dd HH:mm:ss")
@@ -102,6 +107,7 @@ public class EstadisticasActivity extends AppCompatActivity {
                     Map<String, String> params = new HashMap<>();
                     params.put("access_token", acceso.getAccesToken());
 
+                    ArrayList<PieEntry> dataGraficoGuardar = new ArrayList<>();
 
                     Call<Data> callS = service.getEstadisticas(params);
                     callS.enqueue(new Callback<Data>() {
@@ -115,15 +121,11 @@ public class EstadisticasActivity extends AppCompatActivity {
 
                                     System.out.println("Response:\n" + data.getData().get(0).get("name"));
                                     for (int i = 0; i < data.getData().size(); i++) {
-                                        Estadisticas estadisticasSet = new Estadisticas();
-                                        estadisticasSet.setName(data.getData().get(i).get("name").getAsString());
-                                        estadisticasSet.setCantidad(data.getData().get(i).get("cantidad").getAsString());
-                                        estadisticas.add(estadisticasSet);
+                                        dataGraficoGuardar.add(new PieEntry(Integer.parseInt(data.getData().get(i).get("cantidad").getAsString()), data.getData().get(i).get("name").getAsString()));
                                     }
-
-                                    nAdapter = new EstadisticasAdapter(estadisticas);
-                                    mRecyclerView.setAdapter(nAdapter);
-
+                                    pastelData.setValues(dataGraficoGuardar);
+                                    PieData pieData = new PieData(pastelData);
+                                    pieChart.setData(pieData);
                                 } catch (Exception e) {
                                     System.out.println("ERROR: " + e.getMessage());
                                 }
@@ -152,5 +154,9 @@ public class EstadisticasActivity extends AppCompatActivity {
 
             }
         });
+        /////////////////////////////////////////////////////////////
+
     }
+
+
 }
